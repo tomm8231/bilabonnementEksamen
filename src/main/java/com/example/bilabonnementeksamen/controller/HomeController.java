e107113570013481a842b2c8680979ce8b5c3ad8
tomm8231 <tomm8231@stud.kea.dk>
2022-11-24 11:56:22 +0100

    html subpages for leasing created

--- src/main/java/com/example/bilabonnementeksamen/controller/HomeController.java
+++ src/main/java/com/example/bilabonnementeksamen/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
@@ -17,9 +18,31 @@ public class HomeController {
 
   @GetMapping("/registration")
   public String showRegistrationPage() {
-    return "registration-start-page";
+    return "registration-home-page";
   }
 
 
+
+  @GetMapping("/lease-contract")
+  public String showNewContract() {
+    return "lease-start-new-contract";
+  }
+
+  //Find biler ud fra dato og lejetype og send videre
+  @PostMapping("/lease-contract")
+  public String findCarsByDate() {
+    return "redirect:/lease-available-cars";
+  }
+
+  @GetMapping("/lease-available-cars")
+  public String showAvailableCars() {
+    return "lease-available-cars";
+  }
+
+  @PostMapping("/lease-available-cars")
+  public String chooseCar() {
+    //If-statement der tjekker om bilen er reserveret eller ej
+    return "redirect:/lease-find-or-create-customer";
+  }
 
 }
