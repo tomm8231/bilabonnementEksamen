54d5f24411f2bb392f22a3b690379cefd6b0a7d7
SebastianHeiberg <seba3673@stud.kea.dk>
2022-11-30 11:24:25 +0100

    Nu med lidt fungerende biler

--- src/main/java/com/example/bilabonnementeksamen/model/Fuel.java
+++ src/main/java/com/example/bilabonnementeksamen/model/Fuel.java
@@ -12,4 +12,20 @@ public class Fuel {
     this.fuel_type = fuel_type;
     this.distance_unit = distance_unit;
   }
+
+  public String getFuel_type() {
+    return fuel_type;
+  }
+
+  public void setFuel_type(String fuel_type) {
+    this.fuel_type = fuel_type;
+  }
+
+  public String getDistance_unit() {
+    return distance_unit;
+  }
+
+  public void setDistance_unit(String distance_unit) {
+    this.distance_unit = distance_unit;
+  }
 }
