62b24aa888f3eb2bbed41161fde2608e87a4e49c
Skandaniel92 <dani32c4@stud.kea.dk>
2022-12-06 12:50:27 +0100

    Oprettelse af skade, og vis liste af denne

--- src/main/java/com/example/bilabonnementeksamen/model/Problem.java
+++ src/main/java/com/example/bilabonnementeksamen/model/Problem.java
@@ -3,18 +3,20 @@ package com.example.bilabonnementeksamen.model;
 public class Problem {
 
   private int problem_id;
+  private String problem_type;
   private String problem_description;
   private double problem_price;
 
 
   public Problem() {}
 
-  public Problem(int problem_id, String problem_description, double problem_price) {
+
+  public Problem(int problem_id, String problem_type, String problem_description, double problem_price) {
     this.problem_id = problem_id;
+    this.problem_type = problem_type;
     this.problem_description = problem_description;
     this.problem_price = problem_price;
   }
-
 
   public int getProblem_id() {
     return problem_id;
@@ -22,6 +24,14 @@ public class Problem {
 
   public void setProblem_id(int problem_id) {
     this.problem_id = problem_id;
+  }
+
+  public String getProblem_type() {
+    return problem_type;
+  }
+
+  public void setProblem_type(String problem_type) {
+    this.problem_type = problem_type;
   }
 
   public String getProblem_description() {
