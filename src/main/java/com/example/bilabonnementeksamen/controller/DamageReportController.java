8f4770a76fec5fd59bceb43b442150bc497c9391
SebastianHeiberg <seba3673@stud.kea.dk>
2022-12-09 14:07:48 +0100

    vis lejekontrakt

--- src/main/java/com/example/bilabonnementeksamen/controller/DamageReportController.java
+++ src/main/java/com/example/bilabonnementeksamen/controller/DamageReportController.java
@@ -1,15 +1,12 @@
 package com.example.bilabonnementeksamen.controller;
 
-import com.example.bilabonnementeksamen.model.Employee;
 import com.example.bilabonnementeksamen.model.Problem;
-import com.example.bilabonnementeksamen.model.ProblemReport;
 import com.example.bilabonnementeksamen.model.Reservation;
 import com.example.bilabonnementeksamen.service.DamageReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
